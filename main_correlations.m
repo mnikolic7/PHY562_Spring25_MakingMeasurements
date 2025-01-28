0e58751416794d87c5f646b48fecfe5db48d5325
Milos Nikolic <mnikolic@umd.edu>
2025-01-28 14:52:47 -0500

    iniital organization jan28

--- main_correlations.m
+++ main_correlations.m
@@ -4,17 +4,20 @@ load('pair_rule_expression_data.mat');
 %% inspect data
 figure(1)
 subplot(3,1,1)
-imagesc(profiles_eve);
+pcolor(profiles_eve);
+shading flat
 xlabel('Embryo #');
 ylabel('x_{AP}')
 title('Eve');
 subplot(3,1,2)
-imagesc(profiles_run);
+pcolor(profiles_run);
+shading flat
 xlabel('Embryo #');
 ylabel('x_{AP}')
 title('Run');
 subplot(3,1,3)
-imagesc(profiles_prd);
+pcolor(profiles_prd);
+shading flat
 xlabel('Embryo #');
 ylabel('x_{AP}')
 title('Prd');
